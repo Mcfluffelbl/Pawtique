c8ca355789802bb82bc5f371e74e7bff41e9e8ef
Mcfluffelbl <linnea.jmb@gmail.com>
2026-05-25 00:50:30 +0200

    feat: add new pages and components for Cats, About, Contact, Cart, and Home

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,24 @@ function Home() {
 }
 
 function Cats() {
-  return <h1>Cats Page</h1>
+  return (
+    <div>
+      <h1>Cats Page</h1>
+      <h2>Here you can find all the cats we have for sale!</h2>
+    </div>
+  )
+}
+
+function AboutPage() {
+  return <h1>About Page</h1>
+}
+
+function ContactPage() {
+  return <h1>Contact Page</h1>
+}
+
+function Cart() {
+  return <h1>Cart Page</h1>
 }
 
 function App() {
@@ -16,11 +33,21 @@ function App() {
         <Link to="/">Home</Link>
         <br />
         <Link to="/cats">Cats</Link>
+        <br />
+        <Link to="/about">About</Link>
+        <br />
+        <Link to="/contact">Contact</Link>
+        <br />
+        <Link to="/cart">Cart</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cats" element={<Cats />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
 
     </div>
