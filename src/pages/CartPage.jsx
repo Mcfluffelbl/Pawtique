fa7b3cbc0786baa401b63cf53c040cec2fb4f32d
Mcfluffelbl <linnea.jmb@gmail.com>
2026-06-03 21:24:24 +0200

    feat: add empty cart message in CartPage; prompt users to browse and add items

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -43,6 +43,13 @@ function CartPage() {
 
         <div className="cart-items">
 
+          {isEmpty ? (
+          <div className="empty-cart">
+            <h2>Your cart is empty</h2>
+            <p>Browse our adorable cats and add them to your cart!</p>
+          </div>
+        ) : null}
+
           {cart.map((cat) => (
           <div className="cart-item" key={cat.id}>
             <img src={`https://cdn2.thecatapi.com/images/${cat.reference_image_id}.jpg`}
