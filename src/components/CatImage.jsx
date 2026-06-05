397c073f251252646297994b666785df6deb785d
Mcfluffelbl <linnea.jmb@gmail.com>
2026-06-05 03:13:25 +0200

    feat: enhance CatImage component with fallback handling and styling

--- src/components/CatImage.jsx
+++ src/components/CatImage.jsx
@@ -1,17 +1,20 @@
+import { useState } from "react";
 import catSilhouette from "../assets/catsilhouette.png";
 
+import "../styles/CatImage.css";
 
 function CatImage({ src, alt, ...props }) {
+  const [isFallback, setIsFallback] = useState(false);
+
   return (
     <img
-      src={src || catSilhouette}
+      src={isFallback ? catSilhouette : src}
       alt={alt}
-      onError={(e) => {
-        e.target.src = catSilhouette;
-      }}
+      className={isFallback ? "fallback-image" : ""}
+      onError={() => setIsFallback(true)}
       {...props}
     />
-  )
+  );
 }
 
-export default CatImage
+export default CatImage;
